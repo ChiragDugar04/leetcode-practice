a604a3619c1ef16c6dac334cc0c5530e0d48cf53
Chirag Dugar <144232429+ChiragDugar04@users.noreply.github.com>
2025-08-23 21:41:46 +0530

    Time: 5 ms (97.51%) | Memory: 45 MB (31.50%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -2,32 +2,56 @@ class Solution {
     public int evalRPN(String[] tokens) {
         Stack<Integer> stack=new Stack<>();
         for(String token: tokens){
-            if(token.equals("+")){
+            switch(token){
+                case "+" -> {
                 int first=stack.pop();
                 int second=stack.pop();
                 int ans=first+second;
                 stack.push(ans);
-            }
-            else if(token.equals("-")){
+                }
+                case "-"->{
                 int first=stack.pop();
                 int second=stack.pop();
                 int ans=second-first;
                 stack.push(ans);
-            }
-            else if(token.equals("*")){
+                }
+                case "*"->{
                 int first=stack.pop();
                 int second=stack.pop();
                 int ans=second*first;
                 stack.push(ans);
-            }
-            else if(token.equals("/")){
+                }
+                case "/"->{
                 int first=stack.pop();
                 int second=stack.pop();
                 int ans=(int)(second/first);
                 stack.push(ans);
-            }else{
-                stack.push(Integer.parseInt(token));
+                }
+                default -> stack.push(Integer.parseInt(token));
             }
+            // if(token.equals("+")){
+                
+            // }
+            // else if(token.equals("-")){
+            //     int first=stack.pop();
+            //     int second=stack.pop();
+            //     int ans=second-first;
+            //     stack.push(ans);
+            // }
+            // else if(token.equals("*")){
+            //     int first=stack.pop();
+            //     int second=stack.pop();
+            //     int ans=second*first;
+            //     stack.push(ans);
+            // }
+            // else if(token.equals("/")){
+            //     int first=stack.pop();
+            //     int second=stack.pop();
+            //     int ans=(int)(second/first);
+            //     stack.push(ans);
+            // }else{
+            //     stack.push(Integer.parseInt(token));
+            // }
 
         }
         return stack.pop();
