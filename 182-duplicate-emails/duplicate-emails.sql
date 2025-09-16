b10d268092d930e3708ae8921457d8cd2ca83c54
Chirag Dugar <144232429+ChiragDugar04@users.noreply.github.com>
2025-09-16 16:32:24 +0530

    Time: 608 ms (13.46%) | Memory: 0B (100.00%) - LeetSync

--- 182-duplicate-emails/duplicate-emails.sql
+++ 182-duplicate-emails/duplicate-emails.sql
@@ -1,6 +1,10 @@
 /* Write your T-SQL query statement below */
-SELECT 
-    email AS Email
-FROM person
-GROUP BY email
-HAVING COUNT(email)>1
+With t as (
+    Select 
+    p.email as Email,
+    count(*) as count_email
+    From Person p
+    group by email
+    having count(*) > 1
+)
+Select Email from t;
