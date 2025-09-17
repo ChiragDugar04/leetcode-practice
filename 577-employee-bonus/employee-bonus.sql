4ed6115f951d5123be44a64373f555b585ff5d5f
Chirag Dugar <144232429+ChiragDugar04@users.noreply.github.com>
2025-09-17 19:23:06 +0530

    Time: 917 ms (13.87%) | Memory: 0B (100.00%) - LeetSync

---
+++ 577-employee-bonus/employee-bonus.sql
@@ -0,0 +1,9 @@
+/* Write your T-SQL query statement below */
+
+SELECT
+    e.name,
+    b.bonus
+FROM Employee as e
+LEFT JOIN Bonus as b
+ON e.empId=b.empId
+Where b.bonus<1000 OR b.bonus IS NULL;
