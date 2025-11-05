42db1329cc0327b4184b8462c124abb0a3316bf9
Chirag Dugar <144232429+ChiragDugar04@users.noreply.github.com>
2025-11-05 16:17:01 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (83.62%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -6,11 +6,13 @@ class Solution {
         int res=0;
         int i=0;
         while(i<n-1){
-            //local minima
-            while(i<n-1 && prices[i]>=prices[i+1]){i++;}
+            while(i<n-1 && prices[i]>=prices[i+1]){
+                i++;
+            }
             lmin=prices[i];
-            //local maxima
-            while(i<n-1 && prices[i]<=prices[i+1]){i++;}
+            while(i<n-1 && prices[i]<=prices[i+1]){
+                i++;
+            }
             lmax=prices[i];
             res+=(lmax-lmin);
         }
